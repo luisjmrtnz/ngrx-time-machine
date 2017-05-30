73cc4df1a263c05409154e9f7ecfe49b619a3034
John Doe <luisjmartnz@gmail.com>
2017-05-29 23:48:30 -0430

    adding animations to the changes

--- src/app/person.component.ts
+++ src/app/person.component.ts
@@ -1,15 +1,81 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'person',
     template: `
         <div>
             <h2>{{ value.name }}</h2>
-            <p>{{ value.time }}</p>
+            <p class="animated" [class.bounceIn]="hasChanged">{{ value.time }}</p>
         </div>
-    `
+        <ul *ngIf="value.sons">
+            <li *ngFor="let p of value.sons; trackBy: trackById">
+                <person [value]="p"></person>
+            </li>
+        </ul>
+    `,
+    styles: [
+        `   
+            .animated {
+                animation-duration: 0.6s
+            }
+
+            @keyframes bounceIn {
+                0%,100%,20%,40%,60%,80% {
+                    -webkit-animation-timing-function: ease-in;
+                    animation-timing-function: ease-in;
+                }
+
+                0% {
+                    opacity: 0;
+                    -webkit-transform: scale3d(.3,.3,.3);
+                    transform: scale3d(.3,.3,.3)
+                }
+
+                40% {
+                    background-color: blue;
+                    color:#fff;
+                    -webkit-transform: scale3d(.6,.6,.6);
+                    transform: scale3d(.6,.6,.6)
+                }
+
+                70% {
+                    -webkit-transform: scale3d(1.1,1.1,1.1);
+                    transform: scale3d(1.1,1.1,1.1)
+                }
+
+                100% {
+                    opacity: 1;
+                    -webkit-transform: scale3d(1,1,1);
+                    transform: scale3d(1,1,1)
+                }
+            }
+
+            .bounceIn {
+                -webkit-animation-name: bounceIn;
+                animation-name: bounceIn
+            }
+        `
+    ]
 })
 
-export class PersonComponent {
+export class PersonComponent implements OnChanges {
     @Input() value;
+    hasChanged: boolean = false;
+
+    trackById(person) {
+        return person.id;
+    }
+
+    ngOnChanges(changes: SimpleChanges){
+        for(let prop in changes){
+            let chng = changes[prop];
+            let curr = JSON.stringify(chng.currentValue);
+            let prev = JSON.stringify(chng.previousValue);
+
+            if(prev && (curr !== prev)){
+                this.hasChanged = true;
+                setTimeout(() => this.hasChanged = false, 600);
+            }
+        }
+    }
 }
--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -20,27 +20,63 @@ const people = [
     { 
         id: 1,
         name: 'Maria',
-        time: ''
+        time: '',
+        sons: [
+            {
+                id: 100,
+                name: 'Miguel',
+                time: ''
+            },
+            {
+                id: 4,
+                name: 'Jose',
+                time: ''
+            },
+        ]
     },
     { 
         id: 2,
         name: 'Jose',
-        time: ''
+        time: '',
+        sons: []
     },
     { 
         id: 3,
         name: 'Miguel',
-        time: ''
+        time: '',
+        sons: []
     }
 ];
 
 export function peopleReducer(state = people, action: Action){
     switch(action.type) {
         case ClockActions.UPDATE_TIME:
-            let id = action.payload.id;
-            return state.map( person => {
-                return (person.id === id)? Object.assign({}, person, {time: new Date().toString()}) : person;
+            const x = action.payload;
+
+            const updated = state.map( person => {
+             /*  if(person.sons.length > 0) {
+                   return Object.assign({}, person , {
+                        sons: [
+                            {
+                                id: 100,
+                                name: 'Miguel',
+                                time: new Date().toString()
+                            },
+                            {
+                                id: 4,
+                                name: 'Jose',
+                                time: new Date().toString()
+                            },
+                        ]
+                   });
+                }else {
+                    return person;
+                } */
+
+                return (person.id === x.id)? Object.assign({}, person, { time: new Date().toString() }): person;
             });
+
+            return updated;
         default: 
             return state;
     }
